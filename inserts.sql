092d3476a395d088d65db3a54103eff04bd45151
Andrea <andriusmiller59@hotmail.com>
2020-12-22 20:49:23 +0100

    adding first insert of an Hotel

--- inserts.sql
+++ inserts.sql
@@ -8,4 +8,5 @@ INSERT INTO Ciutat(Nom) VALUES ("Zamora");
 INSERT INTO Ciutat(Nom) VALUES ("Cornella de Llobregat");
 INSERT INTO Ciutat(Nom) VALUES ("El prat de Llobregat");
 
-
+INSERT INTO Hotel(Nom,Estels,Adress,Telefon,HabitacionSimple,HabitacionsDobles,DataInaguracio) 
+VALUES ("ATENAS HOTEL",4,"Paseo Josefo 45",934568456,120,80,"05-05-1995");
