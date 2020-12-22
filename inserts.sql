7a8bad2ce0e7ae85b0b86523d331ef87fdf2d93c
Andrea <andriusmiller59@hotmail.com>
2020-12-22 21:33:30 +0100

    add of 4 hotels more

--- inserts.sql
+++ inserts.sql
@@ -10,3 +10,15 @@ INSERT INTO Ciutat(Nom) VALUES ("El prat de Llobregat");
 
 INSERT INTO Hotel(Nom,Estels,Adress,Telefon,HabitacionSimple,HabitacionsDobles,DataInaguracio) 
 VALUES ("ATENAS HOTEL",4,"Paseo Josefo 45",934568456,120,80,"05-05-1995");
+
+INSERT INTO Hotel(Nom,Estels,Adress,Telefon,HabitacionSimple,HabitacionsDobles,DataInaguracio) 
+VALUES ("ATENAS HOTEL",4,"Avenida las Flores 2",937536420,200,49,"05-05-1994");
+
+INSERT INTO Hotel(Nom,Estels,Adress,Telefon,HabitacionSimple,HabitacionsDobles,DataInaguracio) 
+VALUES ("CALIPSO",4,"Calle alcatran 34",934569637,30,10,"05-05-1993");
+
+INSERT INTO Hotel(Nom,Estels,Adress,Telefon,HabitacionSimple,HabitacionsDobles,DataInaguracio) 
+VALUES ("DREAMING CALIFORNIA",4,"Puerta del Sol 90",931237895,50,50,"05-05-1996");
+
+INSERT INTO Hotel(Nom,Estels,Adress,Telefon,HabitacionSimple,HabitacionsDobles,DataInaguracio) 
+VALUES ("DREAMING COLORADO",4,"Puerta de la Luna 90",931000895,30,70,"22-08-2006");
